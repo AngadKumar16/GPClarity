0c9ba2a003699a55ad5a8a987f7649449aa54b43
Angad Kumar <angadkumar16ak@gmail.com>
2026-02-21 16:45:44 -0600

    more iterations in test

--- tests/test_hyperparam_tracker.py
+++ tests/test_hyperparam_tracker.py
@@ -38,7 +38,7 @@ class TestHyperparameterTracker:
     def test_convergence_report(self, simple_gp):
         """Test convergence analysis."""
         tracker = gpclarity.HyperparameterTracker(simple_gp)
-        tracker.wrapped_optimize(max_iters=10)
+        tracker.wrapped_optimize(max_iters=20)
         report = tracker.get_convergence_report()
 
         assert isinstance(report, dict)
